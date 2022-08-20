4b626d8278ab638fbe2ba86e3f1ad5f15a079434
cckn <cckn.dev@gmail.com>
2022-08-20 16:47:19 +0900

    .

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get(':name/:id')
+  getHello(@Param('name') name: string, @Param('id') id: string) {
+    console.log(`name: ${name} typeof: ${typeof name}`);
+    console.log(`id: ${id} type: ${typeof id}`);
+
     return this.appService.getHello();
   }
 
