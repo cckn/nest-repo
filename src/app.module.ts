893bda1f42220d91152e0b9ea523fd0f8e2fc16f
cckn <cckn.dev@gmail.com>
2022-08-22 08:53:42 +0900

    https://wikidocs.net/148984

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
-import { EmailService } from './email/email.service';
+import { CoreModule } from './core/core.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
-  controllers: [UsersController],
-  providers: [UsersService, EmailService],
+  imports: [CoreModule],
+  controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
